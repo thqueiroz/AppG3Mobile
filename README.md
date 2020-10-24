f97f741f4ddced3cad98c25b5d1309fed9f18859
Tiago Henrique Queiroz <tiago.henrique.external@kreios.lu>
2020-10-24 14:29:23 -0300

    Master project

---
+++ README.md
@@ -0,0 +1,7 @@
+# Project builded for the university of Vila Velha for mobile development by Group 3
+
+# How to use:
+ - Git clone this repository.
+ - Open the repository folder
+ - Run yarn for install all dependencies
+ - Run yarn ios/android to run project
---
+++ src/routes/app.routes.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+
+
+const App = createStackNavigator();
+
+const AppRoutes: React.FC = () => (
+  <App.Navigator
+    screenOptions={{
+      headerShown: false,
+      cardStyle: { backgroundColor: '#312e38' },
+    }}
+  >
+    {/* //screens here */}
+    {/* <App.Screen  /> */}
+  </App.Navigator>
+);
+
+export default AppRoutes;
